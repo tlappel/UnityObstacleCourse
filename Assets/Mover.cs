47271468d607c4d04d2c9c2d6112e1099e48e6ab
tlappel <travis@rockcreekcode.com>
2023-01-18 17:46:27 -0600

    added jump

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 2f;
+    [SerializeField] float moveSpeed = 10f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the jungle!");
+        Debug.Log("Move player around with WASD or arrow keys");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue,0,zValue);
-
+        float zJump = Input.GetAxis("Jump");
+        transform.Translate(xValue,zJump,zValue);
     }
 }
